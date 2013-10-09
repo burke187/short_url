665d3986d3d1c3b8ef804a17adac41966d14a9fe
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-09 11:09:52 -0500

    done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,18 @@
 get '/' do
-  # Look in app/views/index.erb
+  
   erb :index
+end
+
+post '/urls' do
+  Url.create(link: params[:link])
+  redirect('/')
+end
+
+get '/:short_link' do
+  url_object = Url.where('short_link=?', params[:short_link])
+  if url_object != []
+    redirect("#{url_object[0].link}")
+  else
+    redirect("/")
+  end
 end
